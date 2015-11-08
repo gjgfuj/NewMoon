da6fc73e06ad4c983deb759a499bc64686d8428d
Sandra <gjgfujb@gmail.com>
2015-11-08 22:35:16 +1100

    Added perspective callback functions.

--- testmod.lua
+++ testmod.lua
@@ -1,2 +1,11 @@
 newmoon.mod.create("testmod")
-newmoon.block.new({name="testblock",onclick=function () print("hi!") end})
+local testblock = newmoon.block.new({name="testblock",onclick=function () print("hi!") end})
+function testblock:voxel()
+    self.texture = newmoon.texture.new("testblock")
+end
+function testblock:side()
+    self.sprite = newmoon.texture.new("testblock_side")
+end
+function testblock:top()
+    self.sprite = newmoon.texture.new("testblock_top")
+end
