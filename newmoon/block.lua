a43713a361de48e17ce36edc7ae9c7fe2e87f0f1
Sandra <gjgfujb@gmail.com>
2016-01-28 23:25:03 +1100

    Brought over changes from SurvivalEngineering.

--- newmoon/block.lua
+++
@@ -1,7 +0,0 @@
-local block = {}
----Create a new block.
----Init functions: voxel() (in a 3d voxel game), top() (in a top-down game), side() (in a side on platformer type game.)
----Callback functions: onUse() when the player activates the block.
-function block.create(b) b.init={} b.callback={} return b end
-newmoon.block = block
-return block
