3cec3bba11f5ba7484e235b8a4484eb4be1f6217
Sandra <gjgfujb@gmail.com>
2016-02-09 15:05:26 +1100

    Fixed loading of mods a bit (textures broken, however.)

--- init.lua
+++ init.lua
@@ -1,10 +1,14 @@
+local oldpackagepath = package.path
+package.path = minetest.get_modpath("newmoon").."/?.lua;"..minetest.get_modpath("newmoon").."/?/init.lua"..oldpackagepath
 _G.newmoon = require("fileloading")
-function newmoon.texture.new(name) return name end
+newmoon.texture = newmoon.helper.optionaltable("newmoon.texture")
+function newmoon.texture.new(name) return name..".png" end
 function newmoon.loadmod(modname) require("mods."..modname) end
 require("mods")
-for modid,mod in newmoon.mod.mods do
-    for id,tile in mod.tiles do
-        tile.init.voxel()
-        minetest.register_node(id,{tiles=tile.texture})
+for modid,mod in pairs(newmoon.mod.mods) do
+    for id,tile in pairs(mod.tiles) do
+        tile.init.voxel(tile)
+        print(tile.texture)
+        minetest.register_node(":"..modid..":"..id,{tiles=tile.texture})
     end
 end
