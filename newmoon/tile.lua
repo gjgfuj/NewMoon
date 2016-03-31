66aac2a92fdc2a49fa376f9adc8926e4d3612393
Sandra <gjgfujb@gmail.com>
2016-04-01 09:26:26 +1100

    Added advanced tile descriptions.

--- newmoon/tile.lua
+++ newmoon/tile.lua
@@ -1,9 +1,12 @@
 local tile = newmoon.helper.optionaltable("newmoon.tile")
 table.insert(newmoon.mod.createcallbacks, function(mod) mod.tiles = {} end)
 ---Create a new tile.
----Object id expected at object.id
+---Tile id expected at tile.id
 ---Init functions: voxel() (in a 3d voxel game), top() (in a top-down game), side() (in a side on platformer type game.)
----Callback functions: None! This is a decorative object.
+---In voxel, this is a stateless block.
+---In top, this is a tile, and the only stats it has is whether it is walkable or not.
+---In side, this is a world block.
+---Callback functions: None! This is a decorative object, and all of it's functions are part of it's material.
 function tile.create(id)
     local b = {}
     b.id = id
